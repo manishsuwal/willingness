7ae1a28d96784cb751e612dd7832a00a15755858
Manish Suwal <enwil410@gmail.com>
2013-12-25 09:41:47 +0545

    Delete trailing spaces

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	
+
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
