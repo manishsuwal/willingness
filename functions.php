736204214a062b0b42c4c6293e1db298bd264e5d
Manish Suwal <enwil410@gmail.com>
2014-01-02 19:45:15 +0545

    Uncomment add theme support post thumbnails

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function willingness_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
@@ -240,7 +240,4 @@ function willingness_body_class($classes) {
 	return $classes;
 }
 
-
-/* Add Theme Support */
-add_theme_support( 'post-thumbnails' );
 
